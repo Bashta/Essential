d361067f6683056bd3b9310654feb6a024e8790e
bashta <erisonve@gmail.com>
2021-08-11 01:51:08 +0200

    Create feed item model

---
+++ FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Erison on 11/08/2021.
+//
+
+import Foundation
+
+struct Feeditem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
