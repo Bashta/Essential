2b99b232167ddca0d950b0592197a9aea2fd97ed
bashta <erisonve@gmail.com>
2021-08-29 01:26:31 +0200

    Fix typo in function neme

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -10,7 +10,7 @@ import EssentialFeed
 
 class CacheFeedUseCaseTests: XCTestCase {
     
-    func test_init_doesNotMessageStoreeUponCreation() {
+    func test_init_doesNotMessageStoreUponCreation() {
         let (_, store) = makeSUT()
         
         XCTAssertEqual(store.receivedMessages, [])
