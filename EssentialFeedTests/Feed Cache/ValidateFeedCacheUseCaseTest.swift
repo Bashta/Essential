851f2f2d152307a823b5a7babd468ddcffa82a40
bashta <erisonve@gmail.com>
2021-08-29 02:32:04 +0200

    Validate cache command does not delete less than seven days old cache

--- EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTest.swift
+++ EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTest.swift
@@ -35,6 +35,19 @@ class ValidateFeedCacheUseCaseTest: XCTestCase {
         XCTAssertEqual(store.receivedMessages, [.retrieve])
     }
     
+    func test_validateCache_doesNotDeleteLessThanSevenDaysOldCache() {
+        let feed = uniqueImageFeed()
+        let fixedCurrentDate = Date()
+        let lessThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: 7).adding(seconds: 1)
+        
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+        
+        sut.validateCache()
+        store.completeRetreival(with: feed.local, timestamp: lessThanSevenDaysOldTimestamp)
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
+    
     // Mark: - Helpers
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
@@ -47,5 +60,30 @@ class ValidateFeedCacheUseCaseTest: XCTestCase {
     
     private func anyError() -> NSError {
         return NSError(domain: "any error", code: 200)
+    }
+    
+    private func uniqueItem() -> FeedImage {
+        return FeedImage(
+            id: UUID(), description: "any", location: "any", url: anyURL())
+    }
+
+    private func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
+        let models = [uniqueItem(), uniqueItem()]
+        let local = models.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
+        return (models, local)
+    }
+
+    private func anyURL() -> URL {
+        return URL(string: "httpS://a-url.com")!
+    }
+}
+
+private extension Date {
+    func adding(days: Int) -> Date {
+        return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
+    }
+    
+    func adding(seconds: TimeInterval) -> Date {
+        return self + seconds
     }
 }
